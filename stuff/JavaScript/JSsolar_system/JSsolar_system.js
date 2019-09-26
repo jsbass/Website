b117f5349d063ab93111a3cbc71302f962f3502e
Seabass992 <jacob.s.bass@hotmail.com>
2019-09-25 23:09:36 -0500

    performance updates

--- stuff/JavaScript/JSsolar_system/JSsolar_system.js
+++ stuff/JavaScript/JSsolar_system/JSsolar_system.js
@@ -6,69 +6,81 @@ var $=document;
 var play=true;
 var contentSize = 0;
 
-function entity(id,name,tRot,relSize,orbFrac){
+var timeDilationSlider;
+var timeDilationLabel;
+var rotCounter;
+var timeCounter
+
+function entity(id,name,tRot,orbFrac, element){
     this.id=id;
     this.name=name;
-    var rotations=0;
+    this.rotations=0;
     this.rotationTime=tRot;
 	this.orbitFraction = orbFrac;
-	this.relativeSize = relSize;
+    this.element = element;
     
-    this.getRots = function(){
-        return rotations;
-    }
-    
-    this.setRots = function(rots){
-        rotations=(rots);
-    }
+    console.log(this);
 };
 
-var mercury = new entity("mercury-orbit","Mercury",88,1,.1);
-var venus = new entity("venus-orbit","Venus",225,.2);
-var earth = new entity("earth-orbit","Earth",365,.3);
-var moon = new entity("moon-orbit","Moon",29,-1);
-var mars = new entity("mars-orbit","Mars",687,.4);
-var asteroid = new entity("asteroid-belt","Asteroid Belt",100,.5);
-var jupiter = new entity("jupiter-orbit","Jupiter",4332,.6);
-var saturn = new entity("saturn-orbit","Saturn",10760,.7);
-var uranus = new entity("uranus-orbit","Uranus",30700,.8);
-var neptune = new entity("neptune-orbit","Neptune",60200,.9);
-var pluto = new entity("pluto-orbit","Pluto",90600,1);
-var bodies = [mercury, venus, earth, moon, mars, asteroid, jupiter, saturn, uranus, neptune, pluto];
+var mercury, venus, earth, moon, mars, asteroid, jupiter, saturn, uranus, neptune, pluto;
+var bodies = [];
 
 window.onload = function() {
-	this.setFocus("earth-orbit");
-    setInterval(this.incOrbit, tick);
+    console.log('onload');
+    mercury = new entity("mercury-orbit","Mercury",88,.1, $.getElementById('mercury-orbit'));
+    console.log($.getElementById('venus-orbit'));
+    venus = new entity("venus-orbit","Venus",225,.2, $.getElementById('venus-orbit'));
+    earth = new entity("earth-orbit","Earth",365,.3, $.getElementById('earth-orbit'));
+    moon = new entity("moon-orbit","Moon",29,-1, $.getElementById('moon-orbit'));
+    mars = new entity("mars-orbit","Mars",687,.4, $.getElementById('mars-orbit'));
+    asteroid = new entity("asteroid-belt","Asteroid Belt",100,.5,$.getElementById('asteroid-belt'));
+    jupiter = new entity("jupiter-orbit","Jupiter",4332,.6, $.getElementById('jupiter-orbit'));
+    saturn = new entity("saturn-orbit","Saturn",10760,.7, $.getElementById('saturn-orbit'));
+    uranus = new entity("uranus-orbit","Uranus",30700,.8, $.getElementById('uranus-orbit'));
+    neptune = new entity("neptune-orbit","Neptune",60200,.9, $.getElementById('neptune-orbit'));
+    pluto = new entity("pluto-orbit","Pluto",90600,1, $.getElementById('pluto-orbit'));
+    bodies = [mercury, venus, earth, moon, mars, asteroid, jupiter, saturn, uranus, neptune, pluto];
+
+    timeDilationSlider = $.getElementById("timeSlider");
+    rotCounter = $.getElementById("rotCounter");
+    timeCounter = $.getElementById("timeCounter");
+    timeDilationLabel = $.getElementById("timeDilation");
+
 	this.onResize();
+    this.setFocus("earth-orbit");
+    requestAnimationFrame(loop);
 };
 
 function onResize(){
 	contentSize = $.getElementById("content").offsetHeight;
 	console.log("content height: " + contentSize);
-	for(obj in bodies){
-		console.log(obj + " height set to " + contentSize*bodies[obj].orbitFraction)
-		$.getElementById(bodies[obj].id).style.height = (contentSize*bodies[obj].orbitFraction) + "px";
-	}
+	// for(obj in bodies){
+    //     var body = bodies[obj];
+	// 	console.log(obj + " height set to " + contentSize*body.orbitFraction)
+	// 	body.element.style.height = (contentSize*body.orbitFraction) + "px";
+	// }
 }
 
 function setFocus(id){
-    var planet;
+    var body;
 	for(obj in bodies){
-        if(bodies[obj].id===id){
-            planet=bodies[obj];
-            console.log(planet.id+" found");
+        body = bodies[obj];
+        if(body.id===id){
+            console.log(body.id+" found");
             break;
         }
     }
+
 	if(this.currentFocus!=null){
-		$.getElementById(this.currentFocus.id).style.borderColor="white";
-	}
-	if(this.currentFocus===planet){
+		this.currentFocus.element.style.borderColor="white";
+    }
+    
+	if(this.currentFocus===body){
 		this.currentFocus=null;
 	} else {
-		this.currentFocus = planet;
-        console.log("currentFocus set to "+planet.id);
-		$.getElementById(this.currentFocus.id).style.borderColor="red";
+		this.currentFocus = body;
+        console.log("currentFocus set to "+body.id);
+		this.currentFocus.element.style.borderColor="red";
 	}
     x = $.getElementsByName("planetName");
     for(i=0;i<x.length;i++){
@@ -80,36 +92,38 @@ function setFocus(id){
 function onCheck(box){
     var entity;
     if(box.name==="sun"){
-        entity = sun;
+        $.getElementById('sun').style.visibility = box.checked ? "visible" : "hidden";
     } else if(box.name==="asteroid"){
         entity = asteroid;
     } else {
         for(obj in bodies){
-            if(bodies[obj].id===box.name+"-orbit"){
-                entity=bodies[obj];
+            var body = bodies[obj];
+            if(body.id===box.name+"-orbit"){
+                entity=body;
                 break;
             }
         }
     }
     if(box.checked){
-        $.getElementById(entity.id).style.visibility="visible";
+        entity.element.style.visibility="visible";
         console.log(entity.id+": visible");
     } else {
         if(entity===this.currentFocus){
-            this.setFocus(entity.id);
+            var focus = entity.id === earth.id ? mercury : earth;
+            this.setFocus(focus.id);
         }
-        $.getElementById(entity.id).style.visibility="hidden";
+        entity.element.style.visibility="hidden";
         console.log(entity.id+": hidden");
     }
 };
 
 function togglePlayback(button){
+    this.play = !this.play;
     if(this.play===true){
-        this.play=false;
-        button.innerHTML="Play";
-    } else if(this.play===false){
-        this.play=true;
+        requestAnimationFrame(loop);
         button.innerHTML="Pause";
+    } else if(this.play===false){
+        button.innerHTML="Play";
     }
 };
 
@@ -120,79 +134,66 @@ function toggleDirection(button){
         button.innerHTML="Rewind";
     }
     this.timeMultiplier*=-1;
-	this.updateInfo();
+	draw();
 };
 
 function resetSys(){
     this.timeElapsed=0;
     for(n in bodies){
-        bodies[n].setRots(0);
-		$.getElementById(bodies[n].id).style.transform="rotate(0deg)";
+        bodies[n].rotations = 0;
     }
-	this.updateInfo();
+	draw();
 };
 
 function dilateTime(slider){
     this.timeMultiplier=slider.value*Math.sign(this.timeMultiplier);
-	this.updateInfo();
+	draw();
 };
 
-function incTime(obj, direction){
-	var time = this.timeMultiplier+direction*Math.sign(this.timeMultiplier);
-	console.log(time);
-	var slider = $.getElementById("timeSlider");
-    if(Math.sign(this.timeMultiplier)>0){
-        var max = parseInt(slider.getAttribute("max"));
-        var min = parseInt(slider.getAttribute("min"));
+var last=null;
+function loop(now) {
+    var dt = 0;
+    if(last!==null) {
+        dt = now - last;
     }
-    else{
-        var max = -parseInt(slider.getAttribute("min"));
-        var min = -parseInt(slider.getAttribute("max"));
+    last = now;
+    update(dt);
+    draw();
+    if(this.play) {
+        requestAnimationFrame(loop);
+    } else {
+        last = null;
     }
-	if(time>=min){
-		if(time<=max){
-			this.timeMultiplier=time;
-			this.updateInfo();
-		} else {
-			this.timeMultiplier=max;
-			this.updateInfo();
-			throw time+" is outside the time scaling range. Defaulted back to "+this.timeMultiplier;
-		}
-	} else {
-		this.timeMultiplier=min;
-		this.updateInfo();
-		throw time+" is outside the time scaling range. Defaulted back to "+this.timeMultiplier;
-	}
+}
+function update(dt) {
+    for(n in bodies) {
+        obj=bodies[n];
+        obj.rotations = (obj.rotations+((1/obj.rotationTime)*(dt/1000)*this.timeMultiplier))%1
+    }
+    this.timeElapsed += dt/1000*this.timeMultiplier;
 }
 
-function incOrbit() {
-    if(this.play){
-        if(tick>=1000){
-            console.log(obj);
-        }
-        for(n in bodies){
-            obj=bodies[n];
-            obj.setRots(obj.getRots()+((1/obj.rotationTime)*(tick/1000)*this.timeMultiplier));
-            $.getElementById(obj.id).style.transform="rotate("+obj.getRots()*360+"deg)";
-        }
-		this.timeElapsed+=this.tick*this.timeMultiplier/1000;
-		this.updateInfo();
+function draw() {
+    
+    for(n in bodies) {
+        var obj = bodies[n];
+        obj.element.style.transform="rotate("+obj.rotations*360+"deg)";
     }
-};
-
-function updateInfo() {
+    
 	if(this.currentFocus!=null){
-        $.getElementById("rotCounter").innerHTML=this.currentFocus.getRots().toFixed(4);
+        rotCounter.innerHTML=this.currentFocus.rotations.toFixed(4);
     } else {
-        $.getElementById("rotCounter").innerHTML="";
+        rotCounter.innerHTML="";
     }
-    $.getElementById("timeCounter").innerHTML=this.timeElapsed.toFixed(2);
-	var slider = $.getElementById("timeSlider");
+    timeCounter.innerHTML=this.timeElapsed.toFixed(2);
+	var slider = timeDilationSlider
 	slider.value=Math.abs(timeMultiplier);
 	if(this.timeMultiplier===1||this.timeMultiplier===-1){
 		units = " day/sec";
 	} else {
 		units = " days/sec";
 	};
-	$.getElementById("timeDilation").innerHTML=this.timeMultiplier+units;
+	timeDilationLabel.innerHTML=this.timeMultiplier+units;
 };
+
+
